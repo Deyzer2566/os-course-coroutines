cdc739d852ccf5f175d8364fca0585a5ba2456da
Deyzer2566 <andrew.kozodoy@yandex.ru>
2025-02-19 15:32:56 +0300

    Empty all files to compare with main branch

--- coroutines.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <functional>
-void new_coroutine(std::function<int(int)> func, int param);
-void coroutines_dispatcher();
-void wait();
--- coroutines_config.h
+++
@@ -1,1 +0,0 @@
-#define MAX_POOL_SIZE 5
